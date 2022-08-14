de2ad81f9fb4d860bb790f0330826823ae1d1bc3
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 11:08:30 +0700

    add base button and input

---
+++ src/components/base/base-button.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  color: String,
+  label: String,
+  block: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const buttonClass = computed(() => {
+  const blockClass = props.block ? 'w-full' : '';
+
+  return [blockClass];
+});
+</script>
+
+<template>
+  <button
+    type="button"
+    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+    :class="buttonClass"
+  >
+    <slot>
+      {{ props.label }}
+    </slot>
+  </button>
+</template>
---
+++ src/components/base/index.js
@@ -0,0 +1,2 @@
+export { default as BaseInput } from './base-input.vue';
+export { default as BaseButton } from './base-button.vue';
