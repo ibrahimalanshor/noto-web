8559f2636d0a4ef9581c97a073918b688273c6e2
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 21:00:17 +0700

    add base dot component

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -2,3 +2,4 @@ export { default as BaseInput } from './base-input.vue';
 export { default as BaseButton } from './base-button.vue';
 export { default as BaseTextarea } from './base-textarea.vue';
 export { default as BaseForm } from './base-form.vue';
+export { default as BaseDot } from './base-dot.vue';
