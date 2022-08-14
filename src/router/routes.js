55e85219c3348edb2a584eda7f2f4e9fd6269457
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 17:38:10 +0700

    add router

---
+++ src/router/routes.js
@@ -0,0 +1,20 @@
+import { RouterView } from 'vue-router';
+
+export default [
+  {
+    path: '/',
+    name: 'Home',
+    component: () => import('@/views/home.vue'),
+  },
+  {
+    path: '/note',
+    component: RouterView,
+    children: [
+      {
+        path: 'create',
+        name: 'NoteCreate',
+        component: () => import('@/views/note/note-create.vue'),
+      },
+    ],
+  },
+];
