1269d4df842616f75f0b06865ffee8379c7d2e67
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 15:02:11 +0700

    add light button

--- src/components/base/base-button.vue
+++ src/components/base/base-button.vue
@@ -2,7 +2,10 @@
 import { computed } from 'vue';
 
 const props = defineProps({
-  color: String,
+  color: {
+    type: String,
+    default: 'default',
+  },
   label: String,
   block: {
     type: Boolean,
@@ -11,18 +14,22 @@ const props = defineProps({
 });
 
 const buttonClass = computed(() => {
-  const blockClass = props.block ? 'w-full' : '';
+  const colors = {
+    default:
+      'text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800',
+    light:
+      'text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700',
+  };
 
-  return [blockClass];
+  const blockClass = props.block ? 'w-full' : '';
+  const colorClass = colors[props.color] ?? colors.default;
+
+  return [blockClass, colorClass];
 });
 </script>
 
 <template>
-  <button
-    type="button"
-    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-    :class="buttonClass"
-  >
+  <button type="button" :class="buttonClass">
     <slot>
       {{ props.label }}
     </slot>
