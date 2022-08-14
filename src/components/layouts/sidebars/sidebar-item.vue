06cc07fb179818ed569584e001d0dd92853a5911
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 11:09:03 +0700

    add sitebar component

---
+++ src/components/layouts/sidebars/sidebar-item.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { Icon } from '@vicons/utils';
+
+const props = defineProps({
+  icon: null,
+  label: {
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <li>
+    <a
+      href="#"
+      class="flex items-center p-2 text-base font-normal text-white rounded-lg dark:text-white hover:bg-white hover:text-gray-900 dark:hover:bg-gray-700"
+    >
+      <icon size="16">
+        <component :is="props.icon" />
+      </icon>
+      <span class="ml-3">{{ props.label }}</span>
+    </a>
+  </li>
+</template>
