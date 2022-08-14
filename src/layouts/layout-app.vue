279a3d7cdc9d02e95844d96e8425d80f941a63d0
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 11:09:50 +0700

    add app layout

---
+++ src/layouts/layout-app.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { TheSidebar } from '@/components/layouts';
+</script>
+
+<template>
+  <div>
+    <the-sidebar />
+    <main class="ml-0 md:ml-64">
+      <slot />
+    </main>
+  </div>
+</template>
