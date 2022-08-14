a089f3e2a30a20701523e7e720389a6e6ecc0ff1
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-14 17:40:07 +0700

    add textarea and form

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -1,2 +1,4 @@
 export { default as BaseInput } from './base-input.vue';
 export { default as BaseButton } from './base-button.vue';
+export { default as BaseTextarea } from './base-textarea.vue';
+export { default as BaseForm } from './base-form.vue';
