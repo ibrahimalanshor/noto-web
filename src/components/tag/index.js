594c2babb1ce605c60181b217e2e48e979caae4d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-17 05:50:13 +0700

    add tag delete

--- src/components/tag/index.js
+++ src/components/tag/index.js
@@ -1,2 +1,3 @@
 export { default as TagCreateModal } from './tag-create-modal.vue';
 export { default as TagEditModal } from './tag-edit-modal.vue';
+export { default as TagDeleteConfirm } from './tag-delete-confirm.vue';
