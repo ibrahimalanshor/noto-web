82043ea73cef4bb6666ead5f7d07c3422a911d2c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-17 09:32:28 +0700

    add trash delete confirm

---
+++ src/components/trash/trash-delete-confirm.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { ref, watch } from 'vue';
+import { BaseConfirm } from '@/components/base';
+
+const props = defineProps({
+  text: {
+    type: String,
+    default: 'Are you sure you want to delete this trash?',
+  },
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+});
+const emit = defineEmits(['update:modelValue', 'close']);
+
+const visible = ref(props.modelValue);
+
+const handleClose = () => {
+  emit('update:modelValue', false);
+  emit('close');
+};
+
+watch(
+  () => props.modelValue,
+  () => {
+    visible.value = props.modelValue;
+  }
+);
+</script>
+
+<template>
+  <base-confirm :text="props.text" v-model="visible" v-on:close="handleClose" />
+</template>
