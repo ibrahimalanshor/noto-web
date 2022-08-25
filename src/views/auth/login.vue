5b21ba03c3a7570c4fef5d40b1657dfae5cf2926
ibra <ibrahimalanshor6@gmail.com>
2022-08-25 17:33:55 +0800

    add login process

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,14 +1,59 @@
 <script setup>
+import { computed } from 'vue';
 import { LayoutAuth } from '@/layouts';
-import { BaseForm, BaseButton } from '@/components/base';
+import { BaseForm, BaseButton, BaseAlert } from '@/components/base';
+import { HandledError } from '@/interfaces';
+
+import { useRouter } from 'vue-router';
+import { useLogin } from '@/compose/auth';
+
+const router = useRouter();
+const { error, credential, loading, login, resetError } = useLogin();
+
+const hasError = computed(() => error.value?.status === 401);
+
+const handleCloseAlert = () => resetError();
+const handleSubmit = async () => {
+  try {
+    await login();
+
+    router.push({ name: 'login' });
+  } catch (err) {
+    if (!(err instanceof HandledError)) {
+      alert('something error');
+    }
+  }
+};
 </script>
 
 <template>
   <layout-auth title="Sign in to your account">
-    <form action="#">
-      <base-form label="Email" />
-      <base-form label="Password" />
-      <base-button label="Sign In" class="mb-4" block />
+    <base-alert
+      color="danger"
+      :visible="hasError"
+      :text="error?.message"
+      v-on:close="handleCloseAlert"
+    />
+    <form v-on:submit.prevent="handleSubmit">
+      <base-form
+        label="Email"
+        v-model="credential.email"
+        :color="error?.errors?.email ? 'danger' : ''"
+        :helper="error?.errors?.email?.msg"
+      />
+      <base-form
+        label="Password"
+        v-model="credential.password"
+        :color="error?.errors?.password ? 'danger' : ''"
+        :helper="error?.errors?.password?.msg"
+      />
+      <base-button
+        type="submit"
+        label="Sign In"
+        class="mb-4"
+        block
+        :disabled="loading"
+      />
       <p
         class="text-center text-sm font-light text-gray-500 dark:text-gray-400"
       >
