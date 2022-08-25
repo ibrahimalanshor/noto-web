3d096eeb364f942a9f74f6e4ce28343d04053857
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-26 05:53:40 +0700

    finish logout

--- src/components/profile/profile-logout-confirm.vue
+++ src/components/profile/profile-logout-confirm.vue
@@ -2,6 +2,14 @@
 import { ref, watch } from 'vue';
 import { BaseConfirm } from '@/components/base';
 
+import { useRouter } from 'vue-router';
+import { useToast } from '@/store';
+import { useLogout } from '@/compose/auth';
+
+const router = useRouter();
+const toast = useToast();
+const { loading, logout } = useLogout();
+
 const props = defineProps({
   modelValue: {
     type: Boolean,
@@ -12,6 +20,15 @@ const emit = defineEmits(['update:modelValue', 'close']);
 
 const visible = ref(props.modelValue);
 
+const handleConfirm = async () => {
+  try {
+    await logout();
+
+    router.push({ name: 'Login' });
+  } catch (err) {
+    toast.show('Something Error');
+  }
+};
 const handleClose = () => {
   emit('update:modelValue', false);
   emit('close');
@@ -28,7 +45,9 @@ watch(
 <template>
   <base-confirm
     text="Logout from your account?"
+    :confirm-disabled="loading"
     v-model="visible"
     v-on:close="handleClose"
+    v-on:confirm="handleConfirm"
   />
 </template>
