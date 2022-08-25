c5afcea76095d978d45b39b8cf1f5fc6c474290d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-26 05:37:09 +0700

    add use registeR

---
+++ src/compose/auth/register.js
@@ -0,0 +1,50 @@
+import { reactive, ref } from 'vue';
+import { auth as authApi } from '@/api';
+import { useAuth } from '@/store';
+import { HandledError } from '../../interfaces';
+
+export default () => {
+  const auth = useAuth();
+
+  const error = ref();
+  const credential = reactive({
+    name: null,
+    email: null,
+    password: null,
+    password_confirmation: null,
+  });
+  const loading = ref(false);
+
+  const register = async () => {
+    loading.value = true;
+    error.value = null;
+
+    try {
+      const res = await authApi.register(credential);
+
+      auth.login(res.data.token.accessToken, res.data.token.refreshToken);
+    } catch (err) {
+      if (err.response) {
+        error.value = err.response.data;
+
+        if ([422, 401].includes(err.response.data.status)) {
+          throw new HandledError(err.response.data);
+        }
+      }
+
+      throw err;
+    } finally {
+      loading.value = false;
+    }
+  };
+
+  const resetError = () => (error.value = null);
+
+  return {
+    error,
+    credential,
+    loading,
+    register,
+    resetError,
+  };
+};
