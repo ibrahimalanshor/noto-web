d9da85b45c81337fb9c5958f3ee40c91252d38db
ibra <ibrahimalanshor6@gmail.com>
2022-08-26 16:26:58 +0800

    add tag list page

--- src/views/tag/tag-list.vue
+++ src/views/tag/tag-list.vue
@@ -1,38 +1,45 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
 import { LayoutApp } from '@/layouts';
 import { HeaderMenu } from '@/components/layouts/headers';
-import { BaseButton } from '@/components/base';
+import { BaseButton, BaseState, BaseSkeleton } from '@/components/base';
 import { TagCreateModal } from '@/components/tag';
-import { useRouter } from 'vue-router';
+import { HandledError } from '@/interfaces';
 
-const router = useRouter();
+import { useGetTag } from '@/compose/tag';
 
-const tagCreateModalStateVisible = ref(false);
+const { loading: tagGetLoading, tag: tagData, getTag } = useGetTag();
 
-const colors = ['primary', 'dark', 'danger', 'warning', 'success', 'info'];
-const tags = [
-  'General',
-  'Diary',
-  'Story',
-  'Love',
-  'Programming',
-  'Crush',
-  'Friends',
-  'Job',
-  'Tasks',
-  'Religion',
-  'Music',
-].map((tag, index) => ({
-  id: index + 1,
-  name: tag,
-  color: colors[Math.floor(Math.random() * colors.length)],
-  notesCount: Math.floor(Math.random() * 20) + 1,
-}));
+const tagCreateModalVisible = ref(false);
+const errorState = reactive({
+  visible: false,
+  title: null,
+  text: null,
+});
+
+const setTag = async () => {
+  try {
+    await getTag();
+  } catch (err) {
+    if (!(err instanceof HandledError)) {
+      errorState.visible = true;
+      (errorState.title = 'Something Error'),
+        (errorState.text = 'Something error when displaying data');
+    } else {
+      errorState.visible = true;
+      errorState.title = err.name;
+      errorState.text = err.message;
+    }
+  }
+};
 
 const handleCreate = () => {
-  tagCreateModalStateVisible.value = true;
+  tagCreateModalVisible.value = true;
 };
+
+onMounted(() => {
+  setTag();
+});
 </script>
 
 <template>
@@ -46,22 +53,39 @@ const handleCreate = () => {
       />
       <h1 class="font-bold text-3xl text-gray-900">Tags</h1>
     </div>
-    <div class="p-5 flex flex-wrap gap-2">
-      <router-link
-        :to="{ name: 'TagDetail', params: { id: tag.id } }"
-        v-for="tag in tags"
-        :key="tag.id"
-        custom
-        v-slot="{ navigate }"
-      >
-        <base-button
-          :label="tag.name"
-          :badge="tag.notesCount"
-          :color="tag.color"
-          v-on:click="navigate"
+    <div class="p-5">
+      <base-skeleton v-if="tagGetLoading" />
+      <template v-else>
+        <base-state
+          :title="errorState.title"
+          :text="errorState.message"
+          v-if="errorState.visible"
         />
-      </router-link>
+        <template v-else>
+          <base-state
+            title="Tags Empty"
+            text="Create a new tag"
+            v-if="tagData.count === 0"
+          />
+          <div class="flex flex-wrap gap-2" v-else>
+            <router-link
+              :to="{ name: 'TagDetail', params: { id: tag.id } }"
+              v-for="tag in tagData.rows"
+              :key="tag.id"
+              custom
+              v-slot="{ navigate }"
+            >
+              <base-button
+                :label="tag.name"
+                :badge="tag.notesCount"
+                :color="tag.color"
+                v-on:click="navigate"
+              />
+            </router-link>
+          </div>
+        </template>
+      </template>
     </div>
-    <tag-create-modal v-model="tagCreateModalStateVisible" />
+    <tag-create-modal v-model="tagCreateModalVisible" />
   </layout-app>
 </template>
