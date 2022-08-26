9295e6b4cec6c8edd2b1ea6e6b6549e89c228fdc
ibra <ibrahimalanshor6@gmail.com>
2022-08-26 16:24:57 +0800

    add base state

--- src/components/base/index.js
+++ src/components/base/index.js
@@ -13,3 +13,4 @@ export { default as BaseToast } from './base-toast.vue';
 export { default as BaseSpin } from './base-spin.vue';
 export { default as BaseError } from './base-error.vue';
 export { default as BaseSkeleton } from './base-skeleton.vue';
+export { default as BaseState } from './base-state.vue';
