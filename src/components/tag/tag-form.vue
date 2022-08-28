7b35df32a162169f15d01f8fd440ef4a48426a83
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-28 16:28:12 +0700

    tag form add validation

--- src/components/tag/tag-form.vue
+++ src/components/tag/tag-form.vue
@@ -5,6 +5,7 @@ import { capitalize } from '@/utils';
 
 const props = defineProps({
   modelValue: Object,
+  validation: Object,
 });
 const emit = defineEmits(['update:modelValue', 'submit']);
 
@@ -19,9 +20,16 @@ const setForm = () => {
   form.color = props.modelValue?.color ?? null;
 };
 
-const handleClickColor = (color) => (form.color = color);
+const handleClickColor = (color) => {
+  form.color = color;
+
+  emit('update:modelValue', form);
+};
+const handleChange = () => {
+  emit('update:modelValue', form);
+};
 const handleSubmit = () => {
-  emit('update:modelValue', form.value);
+  emit('update:modelValue', form);
   emit('submit');
 };
 
@@ -37,7 +45,14 @@ watch(
 
 <template>
   <form v-on:submit.prevent="handleSubmit">
-    <base-form label="Name" placeholder="Name" />
+    <base-form
+      label="Name"
+      placeholder="Name"
+      :color="validation?.name ? 'danger' : ''"
+      :helper="validation?.name?.msg"
+      v-model="form.name"
+      v-on:change="handleChange"
+    />
     <base-form label="Color" class="mb-2">
       <div class="space-x-2">
         <base-button
