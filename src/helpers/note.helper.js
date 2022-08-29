7838d3c00c914c17239ca27c31837432e94c5bbf
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2022-08-29 18:23:10 +0700

    change note date formaT

--- src/helpers/note.helper.js
+++ src/helpers/note.helper.js
@@ -1,5 +1,5 @@
 import dayjs from 'dayjs';
 
 export default {
-  formatDate: (date) => (date ? dayjs(date).format('DD/MM/YYYY') : ''),
+  formatDate: (date) => (date ? dayjs(date).format('DD/MM/YYYY HH:mm:ss') : ''),
 };
